a419d735301efcfa8e0b8c700601117f62a1ba97
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2024-05-28 16:29:33 +0200

    impl ReversePolishSequence

--- src/main/kotlin/moe/sgs/kt/shuntingyard/ReversePolishSequence.kt
+++ src/main/kotlin/moe/sgs/kt/shuntingyard/ReversePolishSequence.kt
@@ -3,15 +3,76 @@ package moe.sgs.kt.shuntingyard
 /**
  * Represents a readable sequence of [Token] values
  *
- * @property sequence of [Token] to reorder
+ * @property infixSequence of [Token] in infix order to reorder
  */
-class ReversePolishSequence(val sequence: Sequence<Token>) : Sequence<Token> {
-    override fun iterator() = object : Iterator<Token> {
-
-        override fun hasNext() = false
-        override fun next(): Token {
-            TODO()
+class ReversePolishSequence(val infixSequence: Sequence<Token>) : Sequence<Token> {
+    override fun iterator() = iterator {
+        val operatorStack = ArrayList<Token>()
+        for (token in infixSequence) {
+            when (token) {
+                is Token.Number -> yield(token)
+                is Token.Value -> yield(token)
+                is Token.Function -> operatorStack.add(token)
+                is Token.Assignment -> handleOperator(operatorStack, token)
+                is Token.Operator -> handleOperator(operatorStack, token)
+                is Token.Komma -> handleKomma(operatorStack)
+                is Token.OpenParen -> operatorStack.add(token)
+                is Token.CloseParen -> handleCloseParen(operatorStack)
+                is Token.None -> throw IllegalArgumentException("invalid token 'none'")
+            }
         }
+        do {
+            val topOperator = operatorStack.removeLastOrNull()
+            require(topOperator !is Token.OpenParen) { "mismatched parentheses" }
+            topOperator?.let { yield(it) }
+        } while (topOperator != null)
+    }
+
+    private suspend fun SequenceScope<Token>.handleKomma(operatorStack: ArrayList<Token>) {
+        while (when (operatorStack.lastOrNull()) {
+                is Token.OpenParen -> false
+                null -> false
+                else -> true
+            }
+        ) {
+            yield(operatorStack.removeLast())
+        }
+    }
+
+    private suspend fun SequenceScope<Token>.handleCloseParen(operatorStack: ArrayList<Token>) {
+        while (when (operatorStack.lastOrNull()) {
+                is Token.OpenParen -> false
+                null -> throw IllegalArgumentException("mismatched parentheses")
+                else -> true
+            }
+        ) {
+            yield(operatorStack.removeLast())
+        }
+        require(operatorStack.lastOrNull() is Token.OpenParen)
+        operatorStack.removeLast()
+        if (operatorStack.lastOrNull() is Token.Function) {
+            yield(operatorStack.removeLast())
+        }
+    }
+
+    private suspend fun SequenceScope<Token>.handleOperator(
+        operatorStack: ArrayList<Token>,
+        token: Token.BaseOperator
+    ) {
+        while (when (val topOperator = operatorStack.lastOrNull()) {
+                is Token.Operator -> {
+                    val higherPrecedent = topOperator.precedent > token.precedent
+                    val equalPrecedent = topOperator.precedent == token.precedent
+                    val leftAssociative = token.associativity == Associativity.LeftAssociative
+                    higherPrecedent || (equalPrecedent && leftAssociative)
+                }
+
+                else -> false
+            }
+        ) {
+            yield(operatorStack.removeLast())
+        }
+        operatorStack.add(token)
     }
 }
 
