f43b76f4ae1c962adfceada552e15688a0c58e47
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2024-05-31 21:32:24 +0200

    add TransactionalHashMap

---
+++ src/main/kotlin/moe/sgs/kt/shuntingyard/TransactionalHashMap.kt
@@ -0,0 +1,83 @@
+package moe.sgs.kt.shuntingyard
+
+/**
+ * A [HashMap] backed [MutableMap] implementing [Transactional]
+ *
+ * @param K the type of map keys. The map is invariant in its key type.
+ * @param V the type of map values. The mutable map is invariant in its value type.
+ */
+class TransactionalHashMap<K, V> : MutableMap<K, V>, Transactional {
+    private var hashMap = HashMap<K, V>()
+    private var abortMap = HashMap<K, V>(hashMap)
+    private var isTransactionActive = false
+
+    override fun isTransactionActive() = isTransactionActive
+
+    override fun commit() {
+        if (isTransactionActive) {
+            abortMap = HashMap(hashMap)
+            isTransactionActive = false
+        }
+    }
+
+    override fun abort() {
+        if (isTransactionActive) {
+            hashMap = HashMap(abortMap)
+            isTransactionActive = false
+        }
+    }
+
+    override val size: Int
+        get() = hashMap.size
+    override val entries: MutableSet<MutableMap.MutableEntry<K, V>>
+        get() {
+            isTransactionActive = true
+            return hashMap.entries
+        }
+    override val keys: MutableSet<K>
+        get() {
+            isTransactionActive = true
+            return hashMap.keys
+        }
+    override val values: MutableCollection<V>
+        get() {
+            isTransactionActive = true
+            return hashMap.values
+        }
+
+    override fun clear() {
+        isTransactionActive = true
+        hashMap.clear()
+    }
+
+    override fun containsKey(key: K): Boolean = hashMap.containsKey(key)
+
+    override fun containsValue(value: V): Boolean = hashMap.containsValue(value)
+
+    override fun get(key: K): V? {
+        return when (val elm = hashMap[key]) {
+            null -> null
+            else -> {
+                isTransactionActive = true
+                elm
+            }
+        }
+    }
+
+    override fun isEmpty(): Boolean = hashMap.isEmpty()
+
+    override fun put(key: K, value: V): V? {
+        isTransactionActive = true
+        return hashMap.put(key, value)
+    }
+
+    override fun putAll(from: Map<out K, V>) {
+        isTransactionActive = true
+        hashMap.putAll(from)
+    }
+
+    override fun remove(key: K): V? {
+        isTransactionActive = true
+        return hashMap.remove(key)
+    }
+}
