a171db18c545891c13ca4d72d75741d3e2529ec0
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2024-06-05 17:52:18 +0200

    make rpn usage in solve lazy resolves #2

--- src/main/kotlin/moe/sgs/kt/shuntingyard/Solve.kt
+++ src/main/kotlin/moe/sgs/kt/shuntingyard/Solve.kt
@@ -21,25 +21,28 @@ fun solve(rpn: ReversePolishSequence, state: State): Result<BigDecimal> = tryCat
         "expected ${nameNoPackage<Token.Number>()} or ${nameNoPackage<Token.Value>()} found ${nameNoPackage(it)}"
     }
 
-    val solveStack = rpn.toMutableList()
-    while (solveStack.size > 1) {
-        val noneNumIdx = solveStack.indexOfFirst {
-            when (it) {
-                is Token.Function -> true
-                is Token.Operator -> true
-                is Token.Assignment -> true
-                else -> false
+    val rpnIt = rpn.iterator()
+    val solveStack = mutableListOf<Token>()
+    if (rpnIt.hasNext()) {
+        solveStack.add(rpnIt.next())
+    }
+    while (rpnIt.hasNext() || solveStack.size > 1) {
+        while (rpnIt.hasNext()) {
+            solveStack.add(rpnIt.next())
+            when (solveStack.last()) {
+                is Token.Function -> break
+                is Token.Operator -> break
+                is Token.Assignment -> break
+                else -> continue
             }
         }
-        if (noneNumIdx == -1) {
-            throw InputMismatchException(couldNotFindOperation)
-        }
-        val (tokenConsume, callArguments, function) = when (val token = solveStack[noneNumIdx]) {
+        val operationIdx = solveStack.lastIndex
+        val (tokenConsume, callArguments, function) = when (val token = solveStack.elementAtOrNull(operationIdx)) {
             is Token.Function -> Triple(token.numArgs, token.numArgs, token.function)
             is Token.Operator -> Triple(token.numArgs, token.numArgs, token.function)
             is Token.Assignment -> Triple(2, 1) { args ->
                 val dec = args.first()
-                when (val maybeValue = solveStack[noneNumIdx - 2]) {
+                when (val maybeValue = solveStack[operationIdx - 2]) {
                     is Token.Value -> state.identifiers[maybeValue.string] = dec
                     else -> throw InputMismatchException(
                         "expected ${nameNoPackage<Token.Value>()} found ${nameNoPackage(maybeValue)}"
@@ -51,21 +54,21 @@ fun solve(rpn: ReversePolishSequence, state: State): Result<BigDecimal> = tryCat
 
             else -> throw InputMismatchException(couldNotFindOperation)
         }
-        if (noneNumIdx < callArguments || noneNumIdx < tokenConsume) {
+        if (operationIdx < callArguments || operationIdx < tokenConsume) {
             throw InputMismatchException(
-                "expected ${max(callArguments, tokenConsume)} more tokens only found ${noneNumIdx - 1}"
+                "expected ${max(callArguments, tokenConsume)} more tokens only found ${operationIdx - 1}"
             )
         }
-        val result = function.invoke(solveStack.subList(noneNumIdx - callArguments, noneNumIdx).map {
+        val result = function.invoke(solveStack.subList(operationIdx - callArguments, operationIdx).map {
             when (it) {
                 is Token.Number -> it.value
                 is Token.Value -> state.identifiers.getValue(it.string)
                 else -> throw InputMismatchException(expectedNumOrValFound(it))
             }
         })
-        solveStack[noneNumIdx - tokenConsume] = Token.Number(result)
+        solveStack[operationIdx - tokenConsume] = Token.Number(result)
         for (i in 1..(tokenConsume)) {
-            solveStack.removeAt(noneNumIdx - tokenConsume + 1)
+            solveStack.removeAt(operationIdx - tokenConsume + 1)
         }
     }
     when (val it = solveStack.firstOrNull()) {
